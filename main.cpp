4c88adca278d179ecd7d900f27b1b3c854fd6e55
Артем Дубровский <dubisoft@gmail.com>
2021-08-15 21:43:54 +0400

    Delete scale factor

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "QApplication"
-
+#include "QDebug"
 #include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
-    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
-
     #if QT_VERSION_MAJOR == 5
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     #endif
